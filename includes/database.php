1242bf631d4e9fd1f21293ceb0cbf2dd0685f5cb
Lisandro Arrua <106292851+lisandro-photo@users.noreply.github.com>
2022-07-06 23:42:59 -0500

    Update database.php

--- includes/database.php
+++ includes/database.php
@@ -1,7 +1,7 @@
 <?php
 
 $db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_BD']);
-
+$db->set_charset("utf8");
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
